55837dd0f564a4c5ac6e9d33f034aea8d9c00160
aadamsx <aadams99@gmail.com>
2017-03-13 12:37:31 -0500

    updates with asteroid

--- admin/client/blaze-views/namesOverview.js
+++ admin/client/blaze-views/namesOverview.js
@@ -1,35 +1,20 @@
 import { Template } from "meteor/templating";
 import { ReactiveDict } from "meteor/reactive-dict";
-import { createClass } from "asteroid";
 
 import "./namesOverview.html";
 
-const Asteroid = createClass();
-// Connect to a Meteor backend
-const asteroid = new Asteroid({
-  endpoint: 'ws://localhost:5000/websocket',
-});
-
-
-asteroid.subscribe("NamesOverview");
-
-
 Template.namesOverview.onCreated(function() {
   this.state = new ReactiveDict();
-  asteroid.subscribe("NamesOverview");
-
 
   this.autorun(() => {
-    asteroid.subscribe("NamesOverview");
-
     // this.subscribe("NamesOverview");
   });
 });
 
+
+
 Template.namesOverview.helpers({
   names() {
-    asteroid.subscribe("NamesOverview");
-
     return Names.find({});
   }
 });
