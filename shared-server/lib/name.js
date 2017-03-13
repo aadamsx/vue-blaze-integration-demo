62f6a5f37ea4f77c9837afd538144c3c4f6b5a84
aadamsx <aadams99@gmail.com>
2017-03-12 22:49:24 -0500

    added shared server

---
+++ shared-server/lib/name.js
@@ -0,0 +1,34 @@
+// 
+//
+// NamesSchema = new SimpleSchema({
+//   userId: { type: String, optional: true },
+//   firstName: { type: String, optional: false, defaultValue: null },
+//   middleName: { type: String, optional: false, defaultValue: null },
+//   middleNameDNA: { type: Boolean, optional: true },
+//   lastName: { type: String, optional: false, defaultValue: null },
+//   priorNameUsed: { type: String, optional: false,
+//     autoform: {
+//       options: function () {
+//         return [
+//           {label: "No", value: "No"},
+//           {label: "Yes", value: "Yes"}
+//         ];
+//       }
+//     },
+//     defaultValue: null
+//   },
+//   nameUsed: { type: String, optional: true,
+//     custom: function () {
+//       if (Meteor.isClient) {
+//         if (this.priorNameUsed.value === 'Yes')
+//           return "required";
+//       }
+//     },
+//     defaultValue: null
+//   },
+//   defaultsSet: { type: Boolean, optional: true, defaultValue: false },
+//   createdAt: { type: Date, optional: true, defaultValue: null },
+//   updatedAt: { type: Date, optional: true }
+// });
+//
+// Names.attachSchema(NamesSchema);
---
+++ shared-server/server/collections.js
@@ -0,0 +1,7 @@
+Names = new Mongo.Collection("names");
+
+Names.deny({
+    insert() {return true;},
+    update() {return true;},
+    remove() {return true;}
+});
